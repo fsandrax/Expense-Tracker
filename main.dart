825fb184b1f595188ac0b11de4bd69a733a300a2
fsandrax <sflores1034@gmail.com>
2025-12-26 21:32:32 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'expenses.dart';
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: const Color.fromARGB(255, 5, 99, 125));
+
+void main() {
+  runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(colorScheme: kDarkColorScheme),
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+    ),
+    home: const Expenses(),
+  ));
+}
